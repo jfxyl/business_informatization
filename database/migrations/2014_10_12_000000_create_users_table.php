a82f6f28285b57dc557ab5bfc0bc233d304b1958
vagrant <vagrant@homestead>
2019-01-29 02:32:03 +0000

    user status

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name')->unique();
             $table->string('password');
-            $table->boolean('status')->default(true);
+            $table->boolean('status')->default(true)->comment('1为正常，2为禁用');
             $table->timestamps();
         });
     }
